610e309ab39eba11ee9f9633436c37250ada4013
MaxSamokhin <samokhinmax@gmail.com>
2018-02-25 15:53:21 +0300

    pass test ApacheBench

--- httptest.py
+++ httptest.py
@@ -6,7 +6,7 @@ import httplib
 import unittest
 
 arg_host = "localhost"
-arg_port = 80
+arg_port = 8001
 if len(sys.argv) > 1:
     arg_host = sys.argv[1]
 if len(sys.argv) > 2:
--- public/constant/server.py
+++ public/constant/server.py
@@ -1,7 +1,7 @@
 HOST = '127.0.0.1'
-PORT = '80'
+PORT = '8001'
 COUNT_CPU = 4  # Number of Cores
-SIZE_QUEUE = 512
+SIZE_QUEUE = 1024
 CHUNK = 1024
 
 ROOT_DIR = '/home/max/max/highload/HighLoad'
